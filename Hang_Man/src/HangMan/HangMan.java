bbb924db0e5030217ddd3fc5bce6535f426535b2
abodn70 <abodn70@hotmail.com>
2019-04-19 21:57:08 +0200

    Update HangMan.java

--- Hang_Man/src/HangMan/HangMan.java
+++ Hang_Man/src/HangMan/HangMan.java
@@ -1,14 +1,13 @@
 package HangMan;
+package abod;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
 
-public class HangMan {
+public class startPlay {
 
 	public static void main(String[] args) {
-
+		hangmanMethods.start();
 		
-hangmanMethods.menu();
-}
+
+	}
+
 }
